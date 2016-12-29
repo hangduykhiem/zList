d0e16170b1688070b71a2e20caf21b29e086f8d3
GuyLandry <hangduykhiem@gmail.com>
2016-12-29 14:23:58 +0200

    Base Fragment implementated

--- app/src/main/java/com/hangduykhiem/zlist/core/base/BaseFragment.kt
+++ app/src/main/java/com/hangduykhiem/zlist/core/base/BaseFragment.kt
@@ -2,6 +2,7 @@ package com.hangduykhiem.zlist.core.base
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +12,41 @@ import android.view.ViewGroup
  *
  * Created by deank on 12/26/16.
  */
-abstract class BaseFragment : Fragment() {
+abstract class BaseFragment<T: BaseView, out V:BasePresenter<T>> : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v = inflater!!.inflate(getMainViewId(), container, false)
-
-        return v
+        injectDependencies()
+        Log.d("BaseActivity", "onCreate")
+        val p = getPresenter()
+        setPresenterView()
+        p.initialize()
+        onFragmentInitialized()
+        return inflater!!.inflate(getMainViewId(), container, false)
     }
 
+    /**
+     * Override to provide the Presenter with view.
+     * This method is call before the presenter.initialize()
+     */
+    abstract fun setPresenterView(): Unit
+
+    /**
+     * Get main view Id, to be inflate in the onCreate method
+     */
     abstract fun getMainViewId(): Int
-}
+
+    /**
+     * Return the presenter, to be initialize during onCreate
+     * This presenter should be injected to the activity in the implementation
+     */
+    abstract fun getPresenter(): V
+
+    /**
+     * This method is called after onCreate
+     */
+    abstract fun onFragmentInitialized(): Unit
+
+    /**
+     * Method to inject dependencies. Is also called in onCreate() method
+     */
+    abstract fun injectDependencies(): Unit}
