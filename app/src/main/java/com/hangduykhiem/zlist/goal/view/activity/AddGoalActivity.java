6b666661bd4d4e52f21b1210991f0212fbacd14c
GuyLandry <hangduykhiem@gmail.com>
2017-01-02 20:08:26 +0200

    Add Goal Activity added

---
+++ app/src/main/java/com/hangduykhiem/zlist/goal/view/activity/AddGoalActivity.java
@@ -0,0 +1,41 @@
+package com.hangduykhiem.zlist.goal.view.activity;
+
+import com.hangduykhiem.zlist.core.base.BaseActivity;
+import com.hangduykhiem.zlist.goal.presenter.AddGoalActivityPresenter;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Activity to add your goal
+ *
+ * Created by deank on 1/2/17.
+ */
+public class AddGoalActivity extends BaseActivity<AddGoalActivityView, AddGoalActivityPresenter> {
+
+
+    @Override
+    public void setPresenterView() {
+
+    }
+
+    @Override
+    public int getMainViewId() {
+        return 0;
+    }
+
+    @NotNull
+    @Override
+    public AddGoalActivityPresenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    public void onActivityInitialized() {
+
+    }
+
+    @Override
+    public void injectDependencies() {
+
+    }
+}
